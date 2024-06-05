4b401925031189e490bc3798d4e728d212fe40bb
Ruei-Fa Tseng <74195702+rflos@users.noreply.github.com>
2024-06-05 21:30:10 +0800

    assignment4-1 release

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include <syslog.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -14,9 +16,15 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+	struct thread_data* thread_func_args = (struct thread_data*) thread_param;
+	usleep(thread_func_args -> wait_to_obtain * 1000);
+	pthread_mutex_lock(thread_func_args -> mutex);
+	usleep(thread_func_args -> wait_to_release * 1000);
+	pthread_mutex_unlock(thread_func_args -> mutex);
+	thread_func_args -> thread_complete_success = true;
     return thread_param;
 }
-
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
@@ -28,6 +36,17 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+	struct thread_data* param = (struct thread_data*)malloc(sizeof(struct thread_data));
+	param -> wait_to_obtain = wait_to_obtain_ms;
+	param -> wait_to_release = wait_to_release_ms;
+	param -> mutex = mutex;
+	param -> thread_complete_success = false;
+
+	int ret = pthread_create(thread, NULL, threadfunc, (void*)param);
+	if(ret != 0){
+		return false;
+	}
+    return true;
 }
 
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -14,6 +14,9 @@ struct thread_data{
      * between the start_thread_obtaining_mutex function and
      * your thread implementation.
      */
+	int wait_to_obtain;
+	int wait_to_release;
+	pthread_mutex_t* mutex;
 
     /**
      * Set to true if the thread completed with success, false
